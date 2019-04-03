647e2d4fc0ba84187b9e17bce7c65e2debe87623
Baenker <th.schm@gmx.de>
2019-04-03 20:11:06 +0200

    Update CCU-Firmware.js

--- CCU-Firmware.js
+++ CCU-Firmware.js
@@ -5,11 +5,16 @@
 * 20.03.19 V1.01    Logging optimiert
 * 01.04.19 V1.02    Firmware kann auch für Rasperrymatic überwacht werden
 *                   Umstellung von var auf const und let (by Dutchman)
+* 01.04.19 V1.03    kleinere Anpassungen
+* 03.04.19 V1.04    Firmware kann nun für piVCCU, piVCCU3 und debimatic übverwacht werden und zwar jeweils mit latest oder testing
+*                   Bugfix Variable Version_Internet
+*                   Logging optimiert und für Debugging erweitert
 **************************/
 const logging = true; 
 const debugging = false; 
 const debugging_response = false;       //nur im Fehlerfall auf true. Hiermit wird die Antwort der Internetadresse protokolliert
-const CCU_Version = 2;                  //Hier eine Zahl eintragen: 2 = CCU2 // 3 = CCU3 // 4 = Rasperrymatic
+const CCU_Version = 2;                  //Hier eine Zahl eintragen: 2 = CCU2 // 3 = CCU3 // 4 = Rasperrymatic // 5= pivccu2 lastest 
+//= 6 pivccu3 latest // 7 = debimatic = latest bzw 51, 61 bzw 71 für die jeweilige Testing Version
 //Datenpunkt auswählen wo die installierte Version ersichtlich ist (aus Homematic.Rega Adapter)
 const id_Version_installiert = "hm-rega.0.MEQ0228930.0.FIRMWARE_VERSION"/*hm-rega.0.MEQ0228930.0.FIRMWARE_VERSION*/;
 //Datenpunkt muss manuell angelegt werden. Kann irgendwo angelegt werden. Anschließend hier ersetzen
@@ -48,7 +53,8 @@ let _message_tmp;
 const request = require('request');
 
 function send_pushover_V4 (_device, _message, _titel, _prio) {
-        if (_prio === 0){let pushover_Instanz =  pushover_Instanz0}
+        let pushover_Instanz;
+        if (_prio === 0){pushover_Instanz =  pushover_Instanz0}
         else if (_prio == 1){pushover_Instanz =  pushover_Instanz1}
         else if (_prio == 2){pushover_Instanz =  pushover_Instanz2}
         else {pushover_Instanz =  pushover_Instanz3}
@@ -81,39 +87,60 @@ function send_mail (_message) {
 }
 
 function func_Version(){
-    const Version_Internet = getState(id_Version_Internet).val;
     const ccu2 = 'http://update.homematic.com/firmware/download?cmd=js_check_version&version=12345&product=HM-CCU2&serial=12345';
     const ccu3 = 'http://update.homematic.com/firmware/download?cmd=js_check_version&version=12345&product=HM-CCU3&serial=12345';
     const Raspi = 'https://gitcdn.xyz/repo/jens-maus/RaspberryMatic/master/release/LATEST-VERSION.js?_version_=CURRENT_VERSION';
+    const pivccu2 = 'https://www.pivccu.de/pivccu.latestVersion?serial=ioBroker';
+    const pivccu3 = 'https://www.pivccu.de/pivccu3.latestVersion?serial=ioBroker';
+    const debimatic = 'https://www.debmatic.de/latestVersion?serial=ioBroker';
+    const testing_pivccu = 'https://www.pivccu.de/pivccu.latestVersion?serial=ioBroker';
+    const testing_pivccu3 = 'https://www.pivccu.de/pivccu3.latestVersion?serial=ioBroker';
+    const testing_debimatic = 'https://www.debmatic.de/latestVersion?serial=ioBroker';
+    
+    
     let ccu;
     if(CCU_Version == 3){ccu = ccu3;}
     else if(CCU_Version == 4){ccu = Raspi;}
+    else if(CCU_Version == 5){ccu = pivccu2;}
+    else if(CCU_Version == 6){ccu = pivccu3;}
+    else if(CCU_Version == 7){ccu = debimatic;}
+    else if(CCU_Version == 51){ccu = testing_pivccu2;}
+    else if(CCU_Version == 61){ccu = testing_pivccu3;}
+    else if(CCU_Version == 71){ccu = testing_debimatic;}
     else {ccu = ccu2;}
-    url = ccu;
+    let url = ccu;
 
     request({url : url},
 
         function (error, response, body) {
+            const Version_Internet = getState(id_Version_Internet).val;
             const Version_installiert = (getState(id_Version_installiert).val).trim();
             const Version = body.split("'");
+            
             if(error){
                 log('error: ' + error);
             } else {
+                if(debugging){
+                    log('[DEBUG] ' +'Version installiert: '+Version_installiert);
+                    log('[DEBUG] ' +'Version Internet: '+Version_Internet);
+                    log('[DEBUG] ' +'Version aus URL: '+Version[1]);
+                    log('[DEBUG] ' +'Name aus URL für Version: '+Version[3]);
+                }
                 if(Version_Internet === ''){
                     if(logging){
-                        log('ausgewähltes Objekt leer. Firmware wird erstmalig gesetzt.');
+                        log('ausgewähltes Objekt leer. Firmware wird erstmalig gesetzt. Firmware: '+Version[1] +' Zentrale: ' +Version[3]);
                     }
                     setState(id_Version_Internet,Version[1]);
                 }
                 
                 if(Version_installiert == Version[1]){
                     if(logging){
-                        log('Installierte Firmware '+Version_installiert  +' der CCU ist aktuell');
+                        log('Installierte Firmware '+Version_installiert  +' der CCU ('+Version[3]  +') ist aktuell.');
                     }
                 }
                 else{
                     if(logging){
-                        log('Installierte Firmware '+Version_installiert  +' der CCU ist nicht aktuell. Aktuell verfügbare Version: '+Version[1]);
+                        log('Installierte Firmware '+Version_installiert  +' der CCU ('+Version[3]  +') ist nicht aktuell. Aktuell verfügbare Version: '+Version[1]);
                     }
                     
                     if(Version_Internet == Version[1]){
@@ -125,7 +152,7 @@ function func_Version(){
                             log('[DEBUG] ' +'Installierte Firmware der CCU ist nicht aktuell.');
                         }
                         setState(id_Version_Internet,Version[1]);
-                         _message_tmp = 'Installierte Firmware der CCU ist nicht aktuell. Installiert: ' +Version_installiert +' --- Verfügbare Version: '+Version[1];
+                         _message_tmp = 'Installierte Firmware der CCU ('+Version[3]  +') ist nicht aktuell. Installiert: ' +Version_installiert +' --- Verfügbare Version: '+Version[1];
                         
                          //Push verschicken
                         if(sendpush){
